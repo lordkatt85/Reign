6c22bfd91a62e9a331625c9def328d33891c993e
Surendhran-P <119148273+Surendhran-P@users.noreply.github.com>
2022-12-04 15:24:22 +0530

    User Interface
    This commit includes the user interfacing json file which may be used for integration with gui development

--- request.py
+++ request.py
@@ -2,18 +2,23 @@
 
 import requests
 
+import user_interface
+
 lat = 51.50
 lon = 0.12
 APIKEY = "b903d80ef10ec6f709b4272d8fae5d1a"
 
-response = requests.get("https://api.openweathermap.org/data/2.5/weather?lat="+str(lat)+"&lon="+str(lon)+"&appid="+APIKEY)
+try:
+	response = requests.get("https://api.openweathermap.org/data/2.5/weather?lat="+str(lat)+"&lon="+str(lon)+"&appid="+APIKEY)
 
-#Checks if the authorozisation was done
-print(response)
- 
-if response.status == 200:
+	#Checks if the authorozisation was done
+	print(response)
+
 	with open("weather.json","w+") as file:
 		file.write(response.text)
 		print("completed")
+		print(response.text)
+
+except:
+	print("Network Error... Please try again")
 	
-print(response.text)
