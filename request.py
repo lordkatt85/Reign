c25580b6c8b8ab5973c545e52a95807c9dc7ed2e
Surendhran-P <119148273+Surendhran-P@users.noreply.github.com>
2022-12-04 07:33:52 +0530

    Add files via upload

--- request.py
+++ request.py
@@ -1,10 +1,19 @@
-# Request Agent
-
-# test request, remove when needed
+#Requests
 
 import requests
 
-response = requests.get("https://www.google.com")
+lat = 51.50
+lon = 0.12
+APIKEY = "b903d80ef10ec6f709b4272d8fae5d1a"
 
-print(response.status_code)
-# print(response.text)
+response = requests.get("https://api.openweathermap.org/data/2.5/weather?lat="+str(lat)+"&lon="+str(lon)+"&appid="+APIKEY)
+
+#Checks if the authorozisation was done
+print(response)
+ 
+if response.status == 200:
+	with open("weather.json","w+") as file:
+		file.write(response.text)
+		print("completed")
+	
+print(response.text)
